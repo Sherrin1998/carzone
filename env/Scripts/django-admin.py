72b8fbd2e632fc964751a5c7e6af71a1e4cc2f95
Sherrin Reji <sherrinreji1998@gmail.com>
2023-09-12 11:25:11 +0530

    my first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sherrin reji\desktop\udemy tutorial\django wd\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
